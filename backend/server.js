779fbeba1a63d4ac47a444e5df71f629caed87f9
sri charan bukka <charan.bukka@gmail.com>
2025-06-01 23:51:37 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,11 @@
 const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.error("MongoDB connection error:", err));
-
 app.get("/", (req, res) => {
-  res.send("My Server is running!!!");
+  res.send("Server is running successfully!!!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
