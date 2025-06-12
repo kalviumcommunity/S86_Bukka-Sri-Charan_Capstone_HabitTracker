2c879f1fe56b549f405f023faa01574a878a0f29
sri charan bukka <charan.bukka@gmail.com>
2025-06-12 21:01:45 +0530

    done with POST endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const habitRoutes = require("./routes/habits"); 
+const userRoutes = require("./routes/user");
 require("dotenv").config();
 
 const app = express();
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api/habits", habitRoutes); 
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("My Server is running!!!");
